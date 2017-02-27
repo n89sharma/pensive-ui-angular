b326e68f24b9b2b7b3c03475d0475e7d293777de
Nikhil Sharma (Mac) <n89sharma@gmail.com>
2017-02-26 23:07:02 -0500

    Pasting data in a note.

--- app/note/note-controller.js
+++ app/note/note-controller.js
@@ -6,14 +6,40 @@ angular.module('note', ['ngRoute', 'ngSanitize'])
     });
   }])
   .controller('NoteController', function(AuthorModel, $http, $sanitize){
+
     var controller = {
         noteData: "",
         noteHtmlData: "",
-        renderMarkdown: renderMarkdown
+        renderMarkdown: renderMarkdown,
+        isHtmlVisible: false,
+        showRawData: showRawData,
+        showHtmlData: showHtmlData,
+        handlePaste: handlePaste
+    };
+
+    function handlePaste (event){
+        console.log('here');
+        var clipboardData = event.clipboardData;
+        angular.forEach(clipboardData.items, function(item, key){
+            console.log(clipboardData.items[key].type);
+            if (clipboardData.items[key]['type'].match(/image.*/)) {
+                var img = clipboardData.items[key].getAsFile();
+                console.log(img.size);
+            }
+        });
     };
 
     function renderMarkdown(){
         controller.noteHtmlData = marked(controller.noteData);
+    }
+
+    function showRawData(){
+        controller.isHtmlVisible  = false;
+    }
+
+    function showHtmlData(){
+        controller.noteHtmlData = marked(controller.noteData);
+        controller.isHtmlVisible  = true;
     }
 
     return controller;
